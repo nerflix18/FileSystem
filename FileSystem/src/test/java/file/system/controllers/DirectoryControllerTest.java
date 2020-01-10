8784a899455c244a75116574e4991049206c5247
1170790 <1170790@isep.ipp.pt>
2020-01-10 19:59:11 +0000

    DirectoryTest class unit tests [refs #8]

--- FileSystem/src/test/java/file/system/controllers/DirectoryControllerTest.java
+++ FileSystem/src/test/java/file/system/controllers/DirectoryControllerTest.java
@@ -97,4 +97,39 @@ public class DirectoryControllerTest {
         assertEquals(expResult, result);
     }
 
+    /**
+     * Test of moveFile method, of class DirectoryController.
+     */
+    @Test
+    public void testMoveFile() {
+        System.out.println("testMoveFile");
+        String path_to_new_dir = "mv_dir";
+        String path_to_file_source = path_to_new_dir + "mv_file";
+        String path_to_file_dest = "mv_file";
+        String path_working_dir = ROOT_TEST;
+        ctrl.createDir(path_to_new_dir, path_working_dir);
+        ctrl.createFile(path_to_file_source, path_working_dir);
+        String expResult = "File moved";
+        String result = ctrl.moveFile(path_to_file_source, path_to_file_dest, path_working_dir);
+        ctrl.deleteFile(path_to_file_dest, path_working_dir);
+        ctrl.deleteDir(path_to_new_dir, path_working_dir);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of moveFile method, of class DirectoryController.
+     */
+    @Test
+    public void testRenameFile() {
+        System.out.println("testRenameFile");
+        String path_to_file_source = "mv_file1";
+        String path_to_file_dest = "mv_file2";
+        String path_working_dir = ROOT_TEST;
+        ctrl.createFile(path_to_file_source, path_working_dir);
+        String expResult = "File moved";
+        String result = ctrl.moveFile(path_to_file_source, path_to_file_dest, path_working_dir);
+        ctrl.deleteFile(path_to_file_dest, path_working_dir);
+        assertEquals(expResult, result);
+    }
+
 }
